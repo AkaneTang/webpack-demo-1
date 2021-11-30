bf6f86f4ee87c901ca38327cf0401630d92df8b2
Akane <865442122@qq.com>
2021-11-30 19:11:20 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Akane",
