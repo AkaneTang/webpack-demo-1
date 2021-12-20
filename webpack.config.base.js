54d511009dd2c1db88e04ef5af7a9e641c921c22
Akane <865442122@qq.com>
2021-12-20 20:03:21 +0800

    准备上传

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,4 +14,46 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          // 把文件变成文件路径
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"], // compiles Styl to CSS
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          "style-loader",
+          // 将 CSS 转化成 CommonJS 模块
+          "css-loader",
+          // 将 Sass 编译成 CSS
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
